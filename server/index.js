c6aa04bcafbcdf45dd50f158139299dc8fd80bf0
Shweta Bharati <shwetabharati039@gmail.com>
2026-03-14 21:48:09 +0530

    update

--- server/index.js
+++ server/index.js
@@ -23,12 +23,12 @@ app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 // FILE STORAGE
-//const storage = multer.diskStorage({
-   // destination: function (req, file, cb) {
-    //    cb(null, 'public/assets');
-    //},
-    //filename: function (req, file, cb) {
-    //    cb(null, file.originalname);
-    //}
-//});
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+       cb(null, 'public/assets');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+}
+});
 const upload = multer({ storage });

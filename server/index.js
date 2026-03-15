c40dd052213c752e5bf6afcfb383a03faf0a94a3
Shweta Bharati <shwetabharati039@gmail.com>
2026-03-15 20:26:04 +0530

    another commit

--- server/index.js
+++ server/index.js
@@ -32,3 +32,13 @@ const storage = multer.diskStorage({
 }
 });
 const upload = multer({ storage });
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
+.catch((error) => console.log(`${error} did not connect`));
